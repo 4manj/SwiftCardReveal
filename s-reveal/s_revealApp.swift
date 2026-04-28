5903665fd8d746a8c6cd87b2d05281c3bc81192d
4manj <amanj2000.aj@gmail.com>
2026-04-28 10:18:28 +0530

    Initial Commit

---
+++ s-reveal/s_revealApp.swift
@@ -0,0 +1,17 @@
+//
+//  s_revealApp.swift
+//  s-reveal
+//
+//  Created by Aman Jain on 28/04/26.
+//
+
+import SwiftUI
+
+@main
+struct s_revealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
